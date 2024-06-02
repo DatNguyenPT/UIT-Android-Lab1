7df6185169cafa4e73736c72efde21b9d68fbfc1
Dat Nguyen <datnpt2004@gmail.com>
2024-06-02 10:20:55 +0700

    EX4 Multi-threading

--- Lab5/app/src/main/java/com/example/myapplication/EX4.kt
+++ Lab5/app/src/main/java/com/example/myapplication/EX4.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.media.MediaPlayer
-import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +11,8 @@ class EX4 : AppCompatActivity() {
     private lateinit var playButton: Button
     private lateinit var stopButton: Button
     private var isPrepared: Boolean = false
+    private var playThread: Thread? = null
+    private var stopThread: Thread? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +26,31 @@ class EX4 : AppCompatActivity() {
         stopButton = findViewById(R.id.stopButton)
 
         playButton.setOnClickListener {
-            PlayMusicTask().execute()
+            playThread = Thread(Runnable {
+                try {
+                    if (!mediaPlayer.isPlaying && !isPrepared) {
+                        mediaPlayer.prepare()
+                    }
+                    mediaPlayer.start()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            })
+            playThread?.start()
         }
 
         stopButton.setOnClickListener {
-            StopMusicTask().execute()
+            stopThread = Thread(Runnable {
+                try {
+                    if (mediaPlayer.isPlaying) {
+                        mediaPlayer.stop()
+                        mediaPlayer.reset() // Reset the MediaPlayer after stopping
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            })
+            stopThread?.start()
         }
     }
 
@@ -39,32 +60,6 @@ class EX4 : AppCompatActivity() {
 
     private fun onMediaCompletion() {
         isPrepared = false
-    }
-
-    inner class PlayMusicTask : AsyncTask<Void, Void, Unit>() {
-        override fun doInBackground(vararg params: Void?) {
-            try {
-                if (!mediaPlayer.isPlaying && !isPrepared) {
-                    mediaPlayer.prepare()
-                }
-                mediaPlayer.start()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    inner class StopMusicTask : AsyncTask<Void, Void, Unit>() {
-        override fun doInBackground(vararg params: Void?) {
-            try {
-                if (mediaPlayer.isPlaying) {
-                    mediaPlayer.stop()
-                    mediaPlayer.reset() // Reset the MediaPlayer after stopping
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
     }
 
     override fun onDestroy() {
